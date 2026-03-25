7cfdcc22c2b19641b0ca46c1ad77dc14e2a479eb
WillerAI <eksosbdib72@gmail.com>
2026-03-25 12:30:00 +0000

    perf: reduce allocs

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,4 @@ pub fn validate_stream(input: &[u8]) -> bool {
 /* commit_ref: 2026-03-20 12:53:00 */
 /* commit_ref: 2026-03-21 14:38:00 */
 /* commit_ref: 2026-03-24 15:11:00 */
+/* commit_ref: 2026-03-25 12:30:00 */
