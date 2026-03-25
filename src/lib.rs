7c0ccb239977752003a94752b86f9bdca3808395
WillerAI <eksosbdib72@gmail.com>
2026-03-25 19:32:00 +0000

    security: add mcp-frame filter

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,4 @@ pub fn validate_stream(input: &[u8]) -> bool {
 /* commit_ref: 2026-03-21 14:38:00 */
 /* commit_ref: 2026-03-24 15:11:00 */
 /* commit_ref: 2026-03-25 12:30:00 */
+/* commit_ref: 2026-03-25 19:32:00 */
