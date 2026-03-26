0e2c27697e64ee2762e47f353256a32af6f8389b
WillerAI <eksosbdib72@gmail.com>
2026-03-26 17:08:00 +0000

    docs: update spec

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,4 @@ pub fn validate_stream(input: &[u8]) -> bool {
 /* commit_ref: 2026-03-24 15:11:00 */
 /* commit_ref: 2026-03-25 12:30:00 */
 /* commit_ref: 2026-03-25 19:32:00 */
+/* commit_ref: 2026-03-26 17:08:00 */
